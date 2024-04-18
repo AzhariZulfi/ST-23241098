900ce28678ebe16009f8531a1e40fa5d93001f01
fyy <147676422+AzhariZulfi@users.noreply.github.com>
2024-04-18 14:35:43 +0800

    Add files via upload

---
+++ tugas_tipe-data.py
@@ -0,0 +1,18 @@
+#tipe data integer
+bil_integer = 1
+print ("Nilai bil_integer =", bil_integer, "jenis tipe datanya =" , type(bil_integer))
+
+#tipe data float
+bil_float = 2.0
+print ("Nilai bil_float =", bil_float, "jenis tipe datanya =" , type(bil_float))
+
+#tipe data string (kumpulan karakter)
+data_string = "my name is azhari"
+print ("Nilai data_string =", data_string, "jenis tipe datanya =" , type(data_string))
+
+# Tipe data boolean
+a = True
+print("Nilai a =", a, "jenis tipe datanya =", type(a))
+
+b = False
+print("Nilai b =", b, "jenis tipe datanya =", type(b))
