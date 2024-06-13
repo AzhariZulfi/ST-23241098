7a424592f0da54af2ed10fd6c51b73613f4b4381
fyy <147676422+AzhariZulfi@users.noreply.github.com>
2024-06-13 14:56:16 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,14 @@
+#belajar multi percabangan
+
+nama = input("masukkan nama: ")
+print(nama)
+if nama == "azhari":
+    print("mahasiswa ganteng")
+
+#elif digunakan diantara if dan else
+elif nama == "haqi":
+    print("mahasiswa ganteng dikit")
+
+else:
+    print("bukan siapa-siapa")
+print("program selesai")
